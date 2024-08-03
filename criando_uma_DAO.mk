47bd547d09f66e85c3c09cc16f9910134ddc3ec8
simao-lab <56369169+simao-lab@users.noreply.github.com>
2024-08-03 19:08:19 -0300

    Add files via upload

---
+++ criando_uma_DAO.mk
@@ -0,0 +1,6 @@
+Projeto de uma DAO(Organização Autônoma Descentralizada), do tipo Philanthropy DAO, para ajudar comunidades carentes no Brasil. O nome do projeto é BrazilAID DAO.
+Missão: Empoderar indivíduos e fortalecer comunidades através da doação voluntária, e livre iniciativa.
+Visão: Se tornar referencia como ferramenta confiável para possibilitar a filantropia nas comunidades brasileiras.
+Valores: Solidariedade, Confiabilidade, Inovação
+Esse projeto, consiste em uma organização que  usa um Token, em que os filantropos e os trabalhadores que empregam seu trabalho ganham quantidade conforme as regras e decisões da organização, os recursos obtidos com as doações são empregados nas comunidades, e as inovações e aplicação de recurso são decididas com base na quantidade de moedas que a os votantes possuem. A aplicação dos recursos são registrados em uma plataforma online que pode ser acessada por quem possui uma pequena quantidade de tokens.
+
